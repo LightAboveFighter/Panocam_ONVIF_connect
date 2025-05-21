d4caad3b52318133f2f8ce1ff76e164a770b32fa
Alexunder <skosarigin@gmail.com>
2025-05-21 20:23:11 +0300

    old lib marshmallow fix

--- src/device_client/request_templates.py
+++ src/device_client/request_templates.py
@@ -6,16 +6,20 @@ from sys import modules
 
 
 def implemented_types():
-    return [cls.__name__.replace("Block", "") for _, cls in getmembers(modules[__name__], isclass) if (cls.__module__ == __name__) and (cls.__name__.count("Block") != 0)] 
+    return [
+        cls.__name__.replace("Block", "")
+        for _, cls in getmembers(modules[__name__], isclass)
+        if (cls.__module__ == __name__) and (cls.__name__.count("Block") != 0)
+    ]
 
 
-#GENERAL templates
+# GENERAL templates
 
 
 class RequestBody(Schema):
     type = fields.String(required=True)
     block = fields.Dict(required=False)
-        
+
     def __init__(self, *args, **kwargs):
         self.allowed_types = implemented_types()
         super().__init__(*args, **kwargs)
@@ -24,20 +28,20 @@ class RequestBody(Schema):
     def check_type(self, type):
         if not type in self.allowed_types:
             raise MarshmallowValidationError(message="Unknown type.", field_name="type")
-        
 
-#STRUCTURES templates
+
+# STRUCTURES templates
 
 
 class CameraNameTemplate(Schema):
     ip = fields.IP(required=True)
-    port = fields.Integer(required=True, allow_string=True, strict=False, validate=Range(0, 65536))
+    port = fields.Integer(required=True, strict=False, validate=Range(0, 65536))
 
     @post_load
     def set_ip_as_str(self, data, *args, **kwargs):
         data["ip"] = str(data["ip"])
         return data
-    
+
 
 class PositionTemplate(Schema):
     x = fields.Float(required=True)
@@ -50,29 +54,42 @@ class SpeedTemplate(Schema):
     y_speed = fields.Float()
     zoom_speed = fields.Float()
 
-    def __init__(self, x_default = 0, y_default = 0, zoom_default = 0, *args, **kwargs):
+    def __init__(self, x_default=0, y_default=0, zoom_default=0, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["x_speed"].load_default = float(x_default)
         self.fields["y_speed"].load_default = float(y_default)
         self.fields["zoom_speed"].load_default = float(zoom_default)
-    
+
     def loads(self, *args, **kwargs):
         loads_result = super().loads(*args, **kwargs)
-        print(abs(loads_result["x_speed"]) + abs(loads_result["y_speed"]) + abs(loads_result["zoom_speed"]))
-        if abs(loads_result["x_speed"]) + abs(loads_result["y_speed"]) + abs(loads_result["zoom_speed"]) == 0:
-            raise MarshmallowValidationError(message="Length of speed vector must be greater than 0.", field_name="(x_speed, y_speed, zoom_speed)")
+        print(
+            abs(loads_result["x_speed"])
+            + abs(loads_result["y_speed"])
+            + abs(loads_result["zoom_speed"])
+        )
+        if (
+            abs(loads_result["x_speed"])
+            + abs(loads_result["y_speed"])
+            + abs(loads_result["zoom_speed"])
+            == 0
+        ):
+            raise MarshmallowValidationError(
+                message="Length of speed vector must be greater than 0.",
+                field_name="(x_speed, y_speed, zoom_speed)",
+            )
         return loads_result
 
 
-#BLOCK templates
+# BLOCK templates
 
-#GENERAL BLOCK templates
+# GENERAL BLOCK templates
+
 
 class NeedsCameraSchema(Schema):
     camera_name = fields.Nested(CameraNameTemplate, many=False, required=True)
 
 
-#SET BLOCK requests templates
+# SET BLOCK requests templates
 
 
 class ConnectionRequestBlock(NeedsCameraSchema):
@@ -88,7 +105,7 @@ class SetHomePositionBlock(NeedsCameraSchema):
     pass
 
 
-#GET BLOCK requests templates
+# GET BLOCK requests templates
 
 
 class GetPositionBlock(NeedsCameraSchema):
@@ -98,12 +115,13 @@ class GetPositionBlock(NeedsCameraSchema):
 class GetLimitsBlock(NeedsCameraSchema):
     pass
 
+
 # class GetRTSPBlock(Schema):
 
 # class GetAvailableCamerasBlock(Schema):
 
 
-#CONTROL BLOCK requests templates
+# CONTROL BLOCK requests templates
 
 
 class ContiniousMoveBlock(NeedsCameraSchema):
@@ -130,7 +148,7 @@ class StopBlock(NeedsCameraSchema):
     stop_zoom = fields.Bool()
 
 
-#DELETE BLOCK requests templates
+# DELETE BLOCK requests templates
 
 
 class CloseConnectionBlock(NeedsCameraSchema):
